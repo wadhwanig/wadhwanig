99c47e5103e79667583210a0d30bfd56d8e89283
Gulshan Wadhwani <wadhwanig@Gulshans-MBP.attlocal.net>
2023-04-03 17:18:12 -0500

    maven|learn

---
+++ src/main/java/HiWorld.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hi world!"));
+        System.out.println("hi world!");
+    }
+}
---
+++ src/test/java/HiWorldTest.java
@@ -0,0 +1,9 @@
+public class HiWorldTest {
+    public void testHi() {
+        System.out.println("TEST - Hi...");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - World!");
+    }
+}
